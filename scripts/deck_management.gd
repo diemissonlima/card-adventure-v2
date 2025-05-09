4deab69ae509bc961f2fcabb7c55e975174acd04
diemisson <diemisson.liima@gmail.com>
2025-05-09 17:47:24 -0400

    passives skills tenacity, battle thirst, last breath e rebirthing adicionadas

--- scripts/deck_management.gd
+++ scripts/deck_management.gd
@@ -5,8 +5,24 @@ class_name DeckManager
 var decks: Dictionary = {
 	"paladin": [
 		"corte", "corte", "corte", "bloquear",
-		"bloquear", "corte_duplo", "fortalecer", "enfraquecer"
+		"bloquear", "corte_duplo", "fortalecer", 
+		"corte_rapido", "corte_tornado", "enfraquecer", "envenenar",
+		"nevoa_venenosa", "pocao_vida"
 	]
 }
 
-# 6 skills passivas adicionadas ao inimigos e todas estao funcionando corretamente
+var passives_description: Dictionary = {
+	"burning fury": "Dano aumentado em +2",
+	"rock hull": "Reduz o dano recebido em 1",
+	"protective shadows": "50% de chance de esquivar o ataque recebido",
+	"tenacity": "Se for receber dano fatal, não morre e fica com 1 de vida",
+	"final fury": "Se a vida estiver abaixo de 50%, o dano aumenta em +4",
+	"vital roots": "Recupera 3 de vida a cada turno",
+	"retaliatory toxin": "Se receber dano direto, causa 1 de veneno",
+	"battle thirst": "A cada 2 ataques, recupera 5 de vida",
+	"last breath": "Ao morrer, causa 10 de dano em todos",
+	"rebirthing": "Ao morrer a primeira vez, revive com 50% de vida"
+}
+
+
+# corrigir bug que o inimigo nao ta morrendo por dano de veneno
